d0867b6a6b22be3ca361380aceec536e0ba0e9a7
Nayan Hajratwala <nayan@lotc.local>
2016-03-19 13:19:05 -0400

    original files

--- src/com/chikli/gdi/help/CashDonation.java
+++ src/com/chikli/gdi/help/CashDonation.java
@@ -1,10 +1,78 @@
 package com.chikli.gdi.help;
 
-public class CashDonation {
-    public CashDonation(int donationId, int donorId, String donationDescription, double donationAmount, String donationDate, String isDeductible, int checkNumber) {
+public class CashDonation extends Donation {
+     /**The check number of the donation*/
+    private int checkNumber;
+
+    public CashDonation(int donationId, int idOfDonor, String donDescription, double donAmt, String dateOfDonation, String deductible,int checkNumber) {
+        super(donationId, idOfDonor, donDescription, donAmt, dateOfDonation, deductible);
+        this.checkNumber = checkNumber;
     }
 
-    public boolean hasValidCheckNumber() {
+ 
+
+    
+    /**Validates that a check is valid by confirming that the check number is greater 
+     * than 100 and less than 5000.
+     * @param numberOfCheck - number of the check that was used to make the donation
+     * @return true if the check number is greater than 100 and less than 5000,
+     * false if it is not.
+     */
+    
+    public boolean validateCheckNumber (int numberOfCheck) {
+        if (checkNumber >= 100 && checkNumber <=5000){
+            return true;     
+        } else {
+            return false;
+    }
+    }
+    
+    
+    /**Sets the check number for a given check.
+     * 
+     * @param noOfCheck - the number of an individual check.
+     */
+    public void setCheckNumber (int noOfCheck) {
+        checkNumber=noOfCheck;
+    }
+    
+    
+    /**Gets the number of an individual check.
+     * 
+     * @return the check number as an integer.
+     */
+    public int getCheckNumber () {
+        return checkNumber;
+    } 
+    
+    public void setDonationId (int donationId) {
+        donationId=donationId;
+    }
+    
+    public int getDonationId () {
+        return donationId;
+    } 
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CashDonation other = (CashDonation) obj;
+        if (this.checkNumber != other.checkNumber) {
+            return false;
+        }
         return true;
     }
+    
+    @Override
+    public String toString() {
+    //    super.toString();
+        return super.toString() + "CashDonation{" + "checkNumber=" + checkNumber + '}';
+    }
+
+    
 }
--- src/com/chikli/gdi/help/PropertyDonation.java
+++ src/com/chikli/gdi/help/PropertyDonation.java
@@ -1,6 +1,58 @@
 package com.chikli.gdi.help;
 
-public class PropertyDonation {
-    public PropertyDonation(int donation_id, int donor_id, String donation_description, double donation_amount, String donation_date, String is_deductible, boolean appraisalPerformed) {
+public class PropertyDonation extends Donation{
+    
+    private boolean appraisalPerformed;
+    
+    /**Construct a property donation with the given values
+     * 
+     * @param donationId - ID number of the donation
+     * @param idOfDonor - ID number of the donor
+     * @param donDescription - description of the donation
+     * @param donAmt - amount of the donation
+     * @param dateOfDonation - date of the donation
+     * @param deductible  - whether the donation is tax deductible
+     * @param appraisal - whether an appraisal of the property was performed
+     */
+    
+    public PropertyDonation(int donationId, int idOfDonor, String donDescription, double donAmt, String dateOfDonation, String deductible, boolean appraisal) {
+        super(donationId, idOfDonor, donDescription, donAmt, dateOfDonation, deductible);
+        appraisalPerformed=appraisal;
     }
+        
+    /**Validates that an appraisal has been performed on a donation of property.
+     * 
+     * @return true if an appraisal has been performed, false if it is has not.
+     */
+    
+    public boolean checkAppraisalPerformed () {
+        if (appraisalPerformed == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**Gets the value of whether an appraisal has been performed on a property donation.
+     * 
+     * return true if an appraisal has been performed, false if it has not.
+     */
+    public boolean isAppraisalPerformed() {
+        return appraisalPerformed;
+    }
+
+    /**Sets the value to true or false depending upon whether an appraisal has been performed.
+     * 
+     * @param appraisalPerformed - whether an appraisal has been performed. 
+     */
+    
+    public void setAppraisalPerformed(boolean appraisalPerformed) {
+        this.appraisalPerformed = appraisalPerformed;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "PropertyDonation{" + "appraisalPerformed=" + appraisalPerformed + '}';
+    }
+
 }
